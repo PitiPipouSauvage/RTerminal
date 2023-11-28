6b8057400339bd0344d36f3b1af4910d963307a2
PitiPipouSauvage <thomas.baert2014@gmail.com>
2023-11-28 19:48:31 +0100

    advancing

--- mysql_server/login.php
+++ mysql_server/login.php
@@ -1,1 +1,14 @@
-<?php ?>
+<?php
+
+$username = $_POST["username"];
+$password = $_POST["pass"];
+
+$mariadb_server_address = 'localhost';
+$mariadb_username = 'phpstorm';
+$mariadb_password = 'TROMPITA2';
+$database_name = 'RTerminal';
+$table_name = 'admins';
+
+$mysqli = mysqli_connect($mariadb_server_address, $mariadb_username, $mariadb_password, $database_name);
+
+$admins_table = mysqli_query($mysqli, "SELECT * FROM admins");
