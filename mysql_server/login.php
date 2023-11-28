a6ddbc52d1e277a308d9a8df0663a8c4a361561b
PitiPipouSauvage <thomas.baert2014@gmail.com>
2023-11-28 21:18:05 +0100

    advancing

--- mysql_server/login.php
+++ mysql_server/login.php
@@ -2,13 +2,48 @@
 
 $username = $_POST["username"];
 $password = $_POST["pass"];
+$doRemember_me = $_POST["remember-me"];
+$isAuthenticated = False;
 
-$mariadb_server_address = 'localhost';
+$encrypted_password = hash('sha512', $password);
+
+$mariadb_server_address = 'jdbc:mariadb://localhost:3306';
 $mariadb_username = 'phpstorm';
 $mariadb_password = 'TROMPITA2';
 $database_name = 'RTerminal';
 $table_name = 'admins';
 
-$mysqli = mysqli_connect($mariadb_server_address, $mariadb_username, $mariadb_password, $database_name);
 
+$mysqli = mysqli_connect($mariadb_server_address, $mariadb_username, $mariadb_password, $database_name);
 $admins_table = mysqli_query($mysqli, "SELECT * FROM admins");
+
+for ($i = 0; count($admins_table); $i++) {
+    if ($admins_table[$i]['username'] === $username and $admins_table[$i]['password'] === $encrypted_password) {
+        $isAuthenticated = True;
+        break;
+    }
+}
+
+setcookie("authentication", $isAuthenticated, time()+3600);
+if ($doRemember_me) {
+    setcookie("authentication", $isAuthenticated);
+}
+
+if ($isAuthenticated === False) {
+    header("./Login_v3/index.html");
+} else {
+    header("./index.php");
+}
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+    <head><title>poop</title></head>
+    <body>
+
+<?php
+echo "poop";
+exit;
+?>
+    </body>
+</html>
