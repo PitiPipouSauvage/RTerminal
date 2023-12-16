478483f2126538ba6cf52b6623c33a894b6f04b6
PitiPipouSauvage <thomas.baert2014@gmail.com>
2023-12-16 17:23:07 +0100

    advancing

--- Bots/level_4/terminal.py
+++ Bots/level_4/terminal.py
@@ -1,9 +1,5 @@
 import pwinput
-import os
-import socket
-import subprocess
 import sys
-import threading 
 
 from Encrypt import encrypt
 
@@ -11,18 +7,25 @@ encrypted_admin_password = encrypt('hello')
 encrypted_admin_username = encrypt('no227')
 
 
-def authenticate():
-    sys.stdout.write('Enter username : ')
-    for line in sys.stdin:
-        username = line
-        break
+def authenticate() -> bool:
+    for attempts in range(3):
+        sys.stdout.write('Enter username : ')
+        sys.stdout.flush()
+        for line in sys.stdin:
+            username = line
+            break
 
-    password = pwinput.pwinput(prompt='Enter password : ')
-    print(password)
+        username = username.split('\n')[0]
+
+        password = pwinput.pwinput(prompt='Enter password : ')
+
+        if encrypt(username) == encrypted_admin_username and encrypt(password) == encrypted_admin_password:
+            return True
+    return False
 
 
 def main():
-    with open('./bin_addr.txt', 'w') as bin_addr:
+    with open('bin_addr.txt', 'w') as bin_addr:
         all_commands = bin_addr.readlines()
 
     command_dir = {}
@@ -31,9 +34,11 @@ def main():
 
     is_active = True
     while is_active:
-        sys.stdout.write('') 
+        sys.stdout.write(command_dir)
+        sys.stdout.flush()
+        is_active = False
 
 
 if __name__ == '__main__':
     authenticate()
-    os.system('pause')
+    main()
