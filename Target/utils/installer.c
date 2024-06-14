04ba80893a3b259ee424ef7c37f5f76043e7934a
PitiPipouSauvage <thomas.baert2014@gmail.com>
2024-06-15 00:08:15 +0200

    installer update

--- Target/utils/installer.c
+++ Target/utils/installer.c
@@ -1,45 +1,35 @@
-#include "C:\MinGW\include\stdlib.h"
+#include <curl/curl.h>
+#include <stdlib.h>
 #include <stdio.h>
 
-/// OS {0 -> Windows, 1 -> Apple, 2 -> Linux, 3 -> Not supported}
 
-#ifdef _WIN32
-#define OS 0
+#define MAX_FILE_SIZE 5120000
+#define MAX_FILENAME_SIZE 32
 
-#elif __APPLE__
-#define OS 1
 
-#elif __linux__
-#define OS 2
+void downloader(const char* url, const char** file_names) {
+	CURL* curl;
+	for (int i=0; i<sizeof(file_names) / sizeof(file_names[0]); i++) {
+		const char* outfilename = file_names[i];
+		curl = curl_easy_init();
+		if (curl) {
+			FILE* file = fopen(outfilename, "wb");
+			curl_easy_setopt(curl, CURLOPT_WRITEDATA, file);
+			curl_easy_perform(curl);
+			fclose(file);
+		}
+		curl_easy_cleanup(curl);
+		curl_global_cleanup();
+	}
+}
 
-#else
-#define OS 3
-#endif
 
 int main() {
-#ifdef _WIN32
-    const char* download_py = (const char*)"curl --output C:\\Windows\\System32\\python_installer.ps1 https://raw.githubusercontent.com/jtmoon79/PythonEmbed4Win/main/PythonEmbed4Win.ps1";
-    system(download_py);
-    const char* install_py = (const char*)"powershell.exe -noexit \"Start-Process -Verb RunAs powershell .\\python_installer.ps1\"";
-    system(install_py);
-
-#elif __linux__
-    const char* install_python_linux = (const char*)"sudo apt-get install -y python";
-    system(install_python_linux);
-    const char* install_pip = (const char*)"sudo apt-get install -y python-pip";
-    system(install_pip);
-
-#elif __APPLE__
-    const char* install_homebrew = (const char*)"/bin/bash -c \"$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)\"";
-    system(install_homebrew);
-    const char* export = (const char*)"echo \"export PATH=\"/usr/local/opt/python/libexec/bin:$PATH\" >> ~/.profile";
-    system(export);
-    const char* install_python = (const char*)"brew install python";
-    system(install_python);
-
-#endif
-
-    const char* command = (const char*)"pause";
-    system(command);
-    return 0;    
+	const char** files = (const char**)malloc(sizeof(const char*)*2);
+	files[0] = "client";
+	files[1] = "updater";
+	char* url = "127.0.0.1";
+	downloader(url, files);
+	return 0;
 }
+
